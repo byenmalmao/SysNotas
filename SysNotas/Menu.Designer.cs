ed12d9e1d29d222a8e8c736887b1ad1fb9b709ee
yolon <yolon@TRANCY>
2025-04-23 19:53:04 -0400

    Add project files.

---
+++ SysNotas/Menu.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace SysNotas
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Olive;
+            ClientSize = new Size(888, 575);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Menu";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Menu";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
